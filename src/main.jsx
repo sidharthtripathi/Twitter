8431a37e279dd1817674c864a1f60a92b4d88593
sidharth tripathi <sidharthtripathi807@gmail.com>
2023-07-10 09:24:59 +0530

    minor fixes to spinner

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import ReactDOM from "react-dom/client";
 import Tweet from "./UserTweet";
-import User from "./fetchUser";
+
 import randomUser from "./random";
 import { LoadingSpinner } from "./UserTweet";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 async function rendering() {
-  root.render(<LoadingSpinner />);
   const users = [];
   const randomNumber = Math.floor(Math.random() * 8) + 1;
   for (let i = 0; i < randomNumber; i++) {
@@ -22,5 +21,6 @@ async function rendering() {
     );
   }
 }
+root.render(<LoadingSpinner />);
 
 rendering();
